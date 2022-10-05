ff1d27db4a73b3e8c14d23099261a5217acd95b5
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-10-05 17:11:20 +0300

    set up github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind()],
+  site: "https://crazytieguy.github.io",
+  base: "/checkers",
 });
