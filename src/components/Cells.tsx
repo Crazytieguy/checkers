d614a24e25dce8f69ceadfb7f4e9665d0c6829f5
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-10-07 20:14:32 +0300

    needless ref

--- src/components/Cells.tsx
+++ src/components/Cells.tsx
@@ -5,16 +5,14 @@ import { hoveredCellIdx } from "../logic/ui";
 export default function Cells() {
   const isHovering = () => hoveredCellIdx() !== undefined;
   return Array.from({ length: 64 }, (_, idx) => {
-    let self: HTMLDivElement;
     const { rem } = idxToPosition(idx);
     const isDraggedOver = createMemo(() => hoveredCellIdx() === idx);
     return (
       <div
-        ref={self!}
         class="opacity-75 transition-colors"
         classList={{
           "duration-1000": !isHovering(),
-          "duration-300": isHovering(),
+          "duration-150": isHovering(),
           "bg-blue-medium": isDraggedOver(),
           "bg-grey-light": rem === 0 && !isDraggedOver(),
           "bg-grey-medium": rem === 1 && !isDraggedOver(),
