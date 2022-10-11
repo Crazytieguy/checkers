b2d32f96a151deabb4487685b0dba637369b85f2
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-10-12 00:47:21 +0300

    better action button layout

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -2,26 +2,26 @@ import { ai, setAI } from "../logic/ai";
 
 export function ActionButtons(props: { undo: () => void; redo: () => void }) {
   return (
-    <div class="mt-6 grid justify-start gap-4 text-2xl sm:grid-flow-col sm:justify-between">
+    <div class="mt-6 flex w-[var(--board-size)] flex-wrap justify-between gap-4 text-xl">
       <button
-        class="w-24 rounded-md border border-blue-dark bg-grey-medium py-2 text-black transition-transform duration-150 active:scale-95"
+        class="w-20 rounded-md border border-blue-dark bg-grey-medium py-2 text-black transition-transform duration-150 active:scale-95"
         onClick={() => props.undo()}
       >
         Undo
       </button>
       <button
-        class="w-24 rounded-md border border-blue-dark bg-grey-medium py-2 text-black transition-transform duration-150 active:scale-95"
-        onClick={() => props.redo()}
-      >
-        Redo
-      </button>
-      <button
-        class="w-24 rounded-md border border-blue-dark py-2 transition duration-150 active:scale-95"
+        class="w-20 rounded-md border border-blue-dark py-2 transition duration-150 active:scale-95"
         aria-pressed={ai()}
         classList={{ "bg-blue-light": ai(), "bg-grey-medium": !ai() }}
         onClick={() => setAI((v) => !v)}
       >
         🤖
+      </button>
+      <button
+        class="w-20 rounded-md border border-blue-dark bg-grey-medium py-2 text-black transition-transform duration-150 active:scale-95"
+        onClick={() => props.redo()}
+      >
+        Redo
       </button>
     </div>
   );
